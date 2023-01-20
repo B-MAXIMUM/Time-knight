25e1ee03f1dc99f5fbe5193b264b6792922409d3
B-MAXIMUM <bgmundy5223@gmail.com>
2023-01-20 10:48:27 -0800

    final final

---
+++ Time Knight prototype/Assets/Scripts/Help.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+using TMPro;
+using UnityEngine.UI;
+
+public class Help : MonoBehaviour
+{
+    public Button weiner;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void cum()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
